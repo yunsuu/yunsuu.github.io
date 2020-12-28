0870c83ad557ff87a1fbeeb8d3bbb91a0cc4ba5f
yunsuu <ljsql934@gmail.com>
2020-12-28 18:15:20 +0900

    blog posting

--- test.js
+++ test.js
@@ -1,13 +1,8 @@
-const b = {
-    name: '하하',
-    bb(){
-        return this.name;
-    },
-    a: x=>{
-        return this.name
-    }
+function printName() {
+    var lastName = "seo";
+    this.firstName = "jeong kuk"
+    console.log(this.lastName + " " + this.firstName)
 }
-
-console.log(b.bb())
-var name = '윤수'
-console.log(b.a())
+var lastName = "kim";
+printName();               // kim jeong kuk
+var o = new printName();   // undefined jeong kuk
