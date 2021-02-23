dde167093f2d5d3b34945ee99063b86d93845190
yunsuu <ljsql934@gmail.com>
2021-02-23 22:08:11 +0900

    posting 할것들

--- test.js
+++ test.js
@@ -1,6 +1,23 @@
-// for(const i=0; i<10; i++){
-//     console.log("Hello World!")
-// }
+function nSecondsLater(n) {
+  console.log(`🚩 n=${n} Start`);
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(console.log(`✅ ${n} Seconds Later`));
+    }, n * 1000);
+  });
+}
+const arr = [0, 1, 2, 3];
+var promises = '';
+function asyncParallel() {
+  promises = arr.map((n) => nSecondsLater(n));
+  console.log(promises);
+  //   Promise.all(promises).then(() => console.log('promises'));
+}
+asyncParallel();
+setTimeout(() => {
+  console.log('프로미스 결과', promises);
+}, 1000 * 5);
+// https://interacting.tistory.com/162
 
-const i = 0
-i = i + 1
+// promise.all은 병렬로 실행시켜주는 트리거가 아닌 실행잘했나 잘못했나 검사하는 선생님
+// 실제 실행할때는 new Promise로 선언할때 실행됨
